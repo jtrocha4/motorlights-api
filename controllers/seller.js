e14df1522717259dcb180cbdf3773f570407a936
jtrocha4 <jtrocha04@gmail.com>
2023-10-27 12:57:04 -0500

    update(controllers): seller

--- controllers/seller.js
+++ controllers/seller.js
@@ -5,59 +5,59 @@ const router = express.Router()
 
 const Seller = require('../models/Seller')
 
-router.get('/api/seller', async(require, response)=>{
-    try {
-        const data = await Seller.find({})
-        response.json(data)
-    } catch (error) {
-        response.status(500).json({message: 'Error al obtener los datos'}).end()
-    }
+router.get('/api/sellers', async (require, response) => {
+  try {
+    const data = await Seller.find({})
+    response.json(data)
+  } catch (error) {
+    response.status(404).json({ message: 'Error al obtener los datos' }).end()
+  }
 })
 
-router.post('/api/seller', async(require, response)=>{
-    try {
-        const {nombre, identificacion, estado = 1 } = require.body
+router.post('/api/sellers', async (require, response) => {
+  try {
+    const { nombre, identificacion, estado, fechaDeCreacion } = require.body
 
-        const newData = new Seller({
-            nombre,
-            identificacion,
-            estado
-        })
+    const newData = new Seller({
+      estado,
+      fechaDeCreacion,
+      identificacion,
+      nombre
+    })
 
-        const createSeller = await newData.save()
-        response.status(200).json(createSeller)
-
-    } catch (error) {
-        response.json({message: error}).end()
-    }
+    const createSeller = await newData.save()
+    response.status(201).json(createSeller)
+  } catch (error) {
+    response.json({ message: error }).end()
+  }
 })
 
-router.delete('/api/seller/:id', async(require, response)=>{
-    try {
-        
-        const id = require.params.id
-        const deleteData = await Seller.findByIdAndDelete(id)
-        response.status(200).json(deleteData)
-    } catch (error) {
-        response.json({message: error}).end()
-    }
+router.delete('/api/sellers/:id', async (require, response) => {
+  try {
+    const id = require.params.id
+    const deleteData = await Seller.findByIdAndDelete(id)
+    response.status(200).json(deleteData)
+  } catch (error) {
+    response.json({ message: error }).end()
+  }
 })
 
-router.put('/api/seller/:id', async(require, response)=>{
-    try {
-        const id = require.params.id
-        const {nombre, identificacion, estado = 1 } = require.body
+router.put('/api/sellers/:id', async (require, response) => {
+  try {
+    const id = require.params.id
+    const { nombre, identificacion, estado = 1 } = require.body
 
-        const data = {
-            nombre
-        }
-
-        const updateData = await Seller.findByIdAndUpdate(id, data, {new: true} )
-        response.status(200).json(updateData)
-
-    } catch (error) {
-        response.json({message: error}).end()
+    const data = {
+      nombre,
+      identificacion,
+      estado
     }
+
+    const updateData = await Seller.findByIdAndUpdate(id, data, { new: true })
+    response.status(200).json(updateData)
+  } catch (error) {
+    response.json({ message: error }).end()
+  }
 })
 
 module.exports = router
