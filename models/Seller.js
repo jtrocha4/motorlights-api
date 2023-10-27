6f6373562d78bf6ca0679d651808be2097a93aac
jtrocha4 <jtrocha04@gmail.com>
2023-10-27 12:52:41 -0500

    update(models): Seller

--- models/Seller.js
+++ models/Seller.js
@@ -1,20 +1,41 @@
 const mongoose = require('mongoose')
-const {Schema, model} = mongoose
+const { Schema, model } = mongoose
 
 const sellerSchema = new Schema({
-    nombre: String,
-    identificacion: String,
-    // telefono: Number,
-    // correoElectronico: String,
-    estado: Number
-}) 
+  nombre: {
+    type: String,
+    required: true
+  },
+  identificacion: {
+    type: String,
+    required: true
+  },
+  // telefono: {
+  //   type: Number,
+  //   required: true
+  // },
+  // correoElectronico: {
+  //   type: String,
+  //   required: true
+  // },
+  estado: {
+    type: Boolean,
+    default: true,
+    required: true
+  },
+  fechaDeCreacion: {
+    type: Date,
+    default: Date.now(),
+    required: true
+  }
+})
 
 sellerSchema.set('toJSON', {
-    transform: (document, returnedObject) =>{
-        returnedObject.id = returnedObject._id
-        delete returnedObject._id,
-        delete returnedObject.__v
-    }
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
 })
 
 const Seller = model('Seller', sellerSchema)
