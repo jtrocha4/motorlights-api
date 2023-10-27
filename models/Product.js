36421e6b7ee55a596b43e01e360241453afb62b0
jtrocha4 <jtrocha04@gmail.com>
2023-10-27 12:53:13 -0500

    feat(models): Product

---
+++ models/Product.js
@@ -0,0 +1,39 @@
+const mongoose = require('mongoose')
+const { Schema, model } = mongoose
+
+const productSchema = new Schema({
+  codigo: {
+    type: String,
+    required: true
+  },
+  nombre: {
+    type: String,
+    required: true
+  },
+  categoria: {
+    type: String,
+    required: true
+  },
+  estado: {
+    type: Boolean,
+    default: true,
+    required: true
+  },
+  fechaDeCreacion: {
+    type: Date,
+    required: true,
+    default: Date.now()
+  }
+})
+
+productSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
+})
+
+const Product = model('Product', productSchema)
+
+module.exports = Product
