d9f0a0b07e8a9a8805db16ecefa648d7bb88eaab
jtrocha4 <jtrocha04@gmail.com>
2023-10-27 12:51:27 -0500

    feat(models): Customer

---
+++ models/Customer.js
@@ -0,0 +1,51 @@
+const mongoose = require('mongoose')
+const { Schema, model } = mongoose
+
+const customerSchema = new Schema({
+  identificacion: {
+    type: Number,
+    required: true
+  },
+  nombre: {
+    type: String,
+    required: true
+  },
+  telefono: {
+    type: Number,
+    required: true
+  },
+  departamento: {
+    type: String,
+    required: true
+  },
+  municipio: {
+    type: String,
+    required: true
+  },
+  direccion: {
+    type: String,
+    required: true
+  },
+  estado: {
+    type: Boolean,
+    default: true,
+    required: true
+  },
+  fechaDeCreacion: {
+    type: Date,
+    default: Date.now(),
+    required: true
+  }
+})
+
+customerSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
+})
+
+const Customer = model('Customer', customerSchema)
+
+module.exports = Customer
