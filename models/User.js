f383998d396b02b5a8c3d421a08f981558cbf014
jtrocha4 <jtrocha04@gmail.com>
2024-01-11 17:39:17 -0500

    feat(models): User

---
+++ models/User.js
@@ -0,0 +1,44 @@
+const mongoose = require('mongoose')
+const { Schema, model } = mongoose
+
+const userSchema = new Schema({
+  nombreUsuario: {
+    type: String,
+    required: true
+  },
+  nombre: {
+    type: String,
+    required: true
+  },
+  hashContrasena: {
+    type: String,
+    required: true
+  },
+  vendedores: [{
+    type: Schema.Types.ObjectId,
+    ref: 'Seller'
+  }],
+  estado: {
+    type: Boolean,
+    default: true,
+    required: true
+  },
+  fechaDeCreacion: {
+    type: Date,
+    default: Date.now(),
+    required: true
+  }
+})
+
+userSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id
+    delete returnedObject._id
+    delete returnedObject.__v
+    delete returnedObject.hashContrasena
+  }
+})
+
+const User = model('User', userSchema)
+
+module.exports = User
