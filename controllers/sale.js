973baef27b4bdb98e9643f6cc4dc8fea94aecf33
jtrocha4 <jtrocha04@gmail.com>
2024-01-18 16:58:45 -0500

    update(controllers): sale

--- controllers/sale.js
+++ controllers/sale.js
@@ -3,11 +3,15 @@ require('../mongo')
 const express = require('express')
 const router = express.Router()
 
-const Sale = require('../models/Sale')
+const authorizationVerification = require('./requiredAuthorization')
 
-router.get('/api/sales', async (require, response) => {
+const Sale = require('../models/Sale')
+const User = require('../models/User')
+
+router.get('/api/sales', authorizationVerification, async (require, response) => {
+  const userId = require.userId
   try {
-    const data = await Sale.find({})
+    const data = await Sale.find({ usuario: userId })
       .populate('idVendedor', {
         _id: 0,
         fechaDeCreacion: 0,
@@ -23,15 +27,23 @@ router.get('/api/sales', async (require, response) => {
         fechaDeCreacion: 0,
         estado: 0
       })
-    response.json(data)
+      .populate('usuario', {
+        nombreUsuario: 1,
+        nombre: 1
+      })
+    response.status(200).json(data)
   } catch (error) {
     response.status(404).json(error).end()
   }
 })
 
-router.post('/api/sales', async (require, response) => {
+router.post('/api/sales', authorizationVerification, async (require, response) => {
   try {
     const { codigoDeFactura, fecha, mes, anio, idVendedor, idCliente, idProducto, metodoDePago, cantidad, valorUnitario, ventaBruta, descuento, ventaNeta, iva, valorTotal, fechaDeCreacion } = require.body
+
+    const userId = require.userId
+
+    const user = await User.findById(userId)
 
     const newData = new Sale({
       cantidad,
@@ -49,17 +61,20 @@ router.post('/api/sales', async (require, response) => {
       valorUnitario,
       ventaBruta,
       ventaNeta,
-      valorTotal
+      valorTotal,
+      usuario: user._id
     })
 
     const createSale = await newData.save()
+    user.ventas = user.ventas.concat(newData._id)
+    await user.save()
     response.status(201).json(createSale)
   } catch (error) {
     response.status(400).json(error).end()
   }
 })
 
-router.get('/api/sales/:idSeller', async (require, response) => {
+router.get('/api/sales/:idSeller', authorizationVerification, async (require, response) => {
   try {
     const idSeller = require.params.idSeller
     const data = await Sale.find({ idVendedor: idSeller })
