ec9c82783cc0047bedb924106e43edd85c54ec91
jtrocha4 <jtrocha04@gmail.com>
2024-01-18 16:57:23 -0500

    update(controllers): product

--- controllers/product.js
+++ controllers/product.js
@@ -3,29 +3,43 @@ require('../mongo')
 const express = require('express')
 const router = express.Router()
 
-const Product = require('../models/Product')
+const authorizationVerification = require('./requiredAuthorization')
 
-router.get('/api/products', async (require, response) => {
+const Product = require('../models/Product')
+const User = require('../models/User')
+
+router.get('/api/products', authorizationVerification, async (require, response) => {
+  const userId = require.userId
   try {
-    const data = await Product.find({})
+    const data = await Product.find({ usuario: userId }).populate('usuario', {
+      nombreUsuario: 1,
+      nombre: 1
+    })
     response.json(data)
   } catch (error) {
     response.status(404).json({ message: 'Error al obtener los datos' }).end()
   }
 })
 
-router.post('/api/products', async (require, response) => {
+router.post('/api/products', authorizationVerification, async (require, response) => {
   try {
     const { codigo, nombre, categoria, estado, fechaDeCreacion } = require.body
+
+    const userId = require.userId
+
+    const user = await User.findById(userId)
 
     const newData = new Product({
       codigo,
       nombre,
       categoria,
+      usuario: user._id,
       estado,
       fechaDeCreacion
     })
     const createProduct = await newData.save()
+    user.productos = user.productos.concat(newData._id)
+    await user.save()
     response.status(201).json(createProduct)
   } catch (error) {
     response.status(400).json(error).end()
