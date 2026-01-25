0cceb74699cdff320a662312b0cc6136d0ce1048
jtrocha4 <jtrocha04@gmail.com>
2026-01-25 14:51:06 -0500

    feat(controllers): inventoryTurnover

---
+++ controllers/inventoryTurnover.js
@@ -0,0 +1,75 @@
+require('../mongo')
+const authorizationVerification = require('./requiredAuthorization')
+
+const express = require('express')
+const router = express.Router()
+
+const InventoryTurnover = require('../models/InventoryTurnover')
+const User = require('../models/User')
+
+router.get('/api/invetoryTurnover', authorizationVerification, async (require, response) => {
+  const userId = require.userId
+  try {
+    const data = await InventoryTurnover.find({ usuario: userId }).populate('usuario', {
+      nombreUsuario: 1,
+      nombre: 1
+    })
+    response.json(data)
+  } catch (error) {
+    response.status(404).json(error).end()
+  }
+})
+
+router.post('/api/invetoryTurnover', authorizationVerification, async (require, response) => {
+  try {
+    const { nombre, id, meta = 0 } = require.body
+
+    const userId = require.userId
+
+    const user = await User.findById(userId)
+
+    const newData = new InventoryTurnover({
+      nombre,
+      id,
+      meta,
+      usuario: user._id
+    })
+
+    const createInventoryTurnover = await newData.save()
+    user.rotacionInventario = user.rotacionInventario.concat(createInventoryTurnover._id)
+    await user.save()
+    response.status(201).json(createInventoryTurnover)
+  } catch (error) {
+    response.status(400).json(error).end()
+  }
+})
+
+router.delete('/api/invetoryTurnover/:id', authorizationVerification, async (require, response) => {
+  try {
+    const id = require.params.id
+    const deleteData = await InventoryTurnover.findByIdAndDelete(id)
+    response.status(200).json(deleteData)
+  } catch (error) {
+    response.status(400).json(error).end()
+  }
+})
+
+router.put('/api/invetoryTurnover/:id', authorizationVerification, async (require, response) => {
+  try {
+    const id = require.params.id
+    const { nombre, meta = 0 } = require.body
+
+    const data = {
+      nombre,
+      id,
+      meta
+    }
+
+    const updateData = await InventoryTurnover.findByIdAndUpdate(id, data, { new: true })
+    response.status(200).json(updateData)
+  } catch (error) {
+    response.json(error).end()
+  }
+})
+
+module.exports = router
